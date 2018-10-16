25ce3578bc3fc60a078fc3d984ea553ac9c89173
James Meyers <james.meyers@emberex.com>
2018-10-16 13:14:09 -0700

    Re-add clean config.

---
+++ config/test.config.js
@@ -0,0 +1,22 @@
+const uuid = require('uuid/v4');
+const { env } = process;
+
+module.exports = {
+  name: 'transition_gradebook',
+  siteUrl: 'http://localhost:8000/',
+
+  webserver: {
+    port: 'auto',
+    publicDir: './public',
+  },
+
+  database: {
+    dropWhenFinished: true,
+    connection: {
+      database: 'transition_gradebook_test_'  + uuid().replace(/-/g, '_'),
+      user: env['USER'] || 'root',
+      host: 'localhost',
+      password: '',
+    },
+  },
+};
