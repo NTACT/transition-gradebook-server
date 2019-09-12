b1e4c9170f27ea9963caea833234274afddffc0b
Justin Felt <justin.felt@emberex.com>
2019-09-12 11:14:23 -0700

    Fix version cron

--- hooks/versionCron.hook.js
+++ hooks/versionCron.hook.js
@@ -21,9 +21,9 @@ module.exports = {
     return `https://raw.githubusercontent.com/NTACT/transition-gradebook-server/master/${file}`;
   },
 
-  versionEmailTemplate(notes) {
+  versionEmailTemplate(notes, version) {
     return `
-      <p> There is a new version of Transition Gradebook that is available for installation. Please update your app to the latest version. </p>
+      <p> Version ${version} of Transition Gradebook available for installation. Please update your app to the latest version. </p>
       <p> Changes from the latest version: </p>
       <ul>
         ${notes.reduce((acc, note) => `${acc}<li>${note}</li>`, "")}
@@ -41,7 +41,7 @@ module.exports = {
       if (semver.lt(version, packageData.version) && latestVersion !== version) {
         rapid.log('Newer version is available, sending email to admins.');
 
-        const releaseNotes = await this.getFileFromGithub(this.serverRepoURL('release-notes.json')).data;
+        const releaseNotes = (await this.getFileFromGithub(this.serverRepoURL('release-notes.json'))).data;
         const [release] = releaseNotes.filter(note => note.version === packageData.version);
 
         const admins = (await rapid.models.User.query()).filter(user => user.admin);
@@ -51,7 +51,7 @@ module.exports = {
             await rapid.sendMail({
               subject: 'Transition Gradebook - New Version Available',
               to: email,
-              html: this.versionEmailTemplate(release.notes)
+              html: this.versionEmailTemplate(release.notes, release.version)
             });
           } catch (e) {
             rapid.log(`Error sending version update email: ${e}`);
