2f08f0eecc73cf67144408e530208c7c70f15947
Ryan Peterson <ryan.peterson@emberex.com>
2019-09-23 09:49:46 -0700

    Chunk the pre-ETS report students into batches of 6

--- reportTemplates/preEtsReport.js
+++ reportTemplates/preEtsReport.js
@@ -10,6 +10,8 @@ const {
   Component
 } = React;
 
+const chunk = require('lodash/chunk');
+
 const SingleTermReportTitle = require('./components/SingleTermReportTitle');
 
 class PreEtsReport extends Component {
@@ -21,39 +23,16 @@ class PreEtsReport extends Component {
       schoolYear,
       appliedFilters
     } = this.props.data;
+    const studentPages = chunk(inSchoolStudents, 6);
     return React.createElement(React.Fragment, null, React.createElement(SingleTermReportTitle, {
       reportName: "Pre-ETS Activity Report",
       schoolSettings: schoolSettings,
       schoolYear: schoolYear,
       term: term,
       appliedFilters: appliedFilters
-    }), React.createElement("table", {
-      style: {
-        width: '100%',
-        textAlign: 'center',
-        borderCollapse: 'collapse'
-      }
-    }, React.createElement("thead", null, React.createElement("tr", {
-      style: {
-        backgroundColor: '#D43425',
-        color: 'white',
-        width: '100%',
-        fontSize: '50%',
-        padding: '20px 10px 20px 10px'
-      }
-    }, React.createElement("th", null, "Name"), React.createElement("th", null, "Student ID"), React.createElement("th", null, "Race/", React.createElement("br", null), "Ethnicity"), React.createElement("th", null, "Engaged with VR"), React.createElement("th", null, "On-track for graduation"), React.createElement("th", null, "Job Exploration Counseling"), React.createElement("th", null, "Work Based Learning"), React.createElement("th", null, "Counseling on Transition", React.createElement("br", null), "Programs or PSE at IHEs"), React.createElement("th", null, "Training in Workplace Readiness,", React.createElement("br", null), "Social Skills,", React.createElement("br", null), "Independent Living"), React.createElement("th", null, "Instruction in Self-Advocacy"))), React.createElement("tbody", null, inSchoolStudents.map((student, index) => {
-      const engagedWithVr = student.activities.some(activity => activity.activityType.name === 'Referral Complete to VR (PW, IAC)');
-      const hasJobExplorationCounseling = student.activities.some(activity => activity.activityType.name === 'Job Exploration Counseling (CW, Pre-ETS)');
-      const hasWorkBasedLearning = student.activities.some(activity => activity.activityType.activityTypeGroup.name === 'Work Experience');
-      const hasCounselingOnTransitionPrograms = student.activities.some(activity => {
-        const activityTypeName = activity.activityType.name;
-        return activityTypeName === 'Career Mentor (CW, SS)' || activityTypeName === 'Graduation Coach/ Mentor (SS)' || activityTypeName === 'School Counselor for Post-School Planning (SS)';
-      });
-      return React.createElement(TableRow, {
-        key: student.id,
-        index: index
-      }, React.createElement("td", null, student.firstName, " ", student.lastName), React.createElement("td", null, student.studentId), React.createElement("td", null, student.race), React.createElement("td", null, engagedWithVr ? 'Yes' : 'No'), React.createElement("td", null, student.onTrack ? 'Yes' : 'No'), React.createElement("td", null, hasJobExplorationCounseling ? 'Yes' : 'No'), React.createElement("td", null, hasWorkBasedLearning ? 'Yes' : 'No'), React.createElement("td", null, hasCounselingOnTransitionPrograms ? 'Yes' : 'No'), React.createElement("td", null, student.hasIndependentLivingSkills || student.hasSocialSkills ? 'Yes' : 'No'), React.createElement("td", null, student.hasSelfDeterminationSkills ? 'Yes' : 'No'));
-    }))));
+    }), studentPages.map(students => React.createElement(PreETSTable, {
+      inSchoolStudents: students
+    })));
   }
 
 }
@@ -72,6 +51,41 @@ function TableRow(props) {
     }
   }));
 }
+
+const PreETSTable = (_ref) => {
+  let {
+    inSchoolStudents
+  } = _ref,
+      rest = _objectWithoutProperties(_ref, ["inSchoolStudents"]);
+
+  return React.createElement("table", _extends({
+    style: {
+      width: '100%',
+      textAlign: 'center',
+      borderCollapse: 'collapse'
+    }
+  }, rest), React.createElement("thead", null, React.createElement("tr", {
+    style: {
+      backgroundColor: '#D43425',
+      color: 'white',
+      width: '100%',
+      fontSize: '50%',
+      padding: '20px 10px 20px 10px'
+    }
+  }, React.createElement("th", null, "Name"), React.createElement("th", null, "Student ID"), React.createElement("th", null, "Race/", React.createElement("br", null), "Ethnicity"), React.createElement("th", null, "Engaged with VR"), React.createElement("th", null, "On-track for graduation"), React.createElement("th", null, "Job Exploration Counseling"), React.createElement("th", null, "Work Based Learning"), React.createElement("th", null, "Counseling on Transition", React.createElement("br", null), "Programs or PSE at IHEs"), React.createElement("th", null, "Training in Workplace Readiness,", React.createElement("br", null), "Social Skills,", React.createElement("br", null), "Independent Living"), React.createElement("th", null, "Instruction in Self-Advocacy"))), React.createElement("tbody", null, inSchoolStudents.map((student, index) => {
+    const engagedWithVr = student.activities.some(activity => activity.activityType.name === 'Referral Complete to VR (PW, IAC)');
+    const hasJobExplorationCounseling = student.activities.some(activity => activity.activityType.name === 'Job Exploration Counseling (CW, Pre-ETS)');
+    const hasWorkBasedLearning = student.activities.some(activity => activity.activityType.activityTypeGroup.name === 'Work Experience');
+    const hasCounselingOnTransitionPrograms = student.activities.some(activity => {
+      const activityTypeName = activity.activityType.name;
+      return activityTypeName === 'Career Mentor (CW, SS)' || activityTypeName === 'Graduation Coach/ Mentor (SS)' || activityTypeName === 'School Counselor for Post-School Planning (SS)';
+    });
+    return React.createElement(TableRow, {
+      key: student.id,
+      index: index
+    }, React.createElement("td", null, student.firstName, " ", student.lastName), React.createElement("td", null, student.studentId), React.createElement("td", null, student.race), React.createElement("td", null, engagedWithVr ? 'Yes' : 'No'), React.createElement("td", null, student.onTrack ? 'Yes' : 'No'), React.createElement("td", null, hasJobExplorationCounseling ? 'Yes' : 'No'), React.createElement("td", null, hasWorkBasedLearning ? 'Yes' : 'No'), React.createElement("td", null, hasCounselingOnTransitionPrograms ? 'Yes' : 'No'), React.createElement("td", null, student.hasIndependentLivingSkills || student.hasSocialSkills ? 'Yes' : 'No'), React.createElement("td", null, student.hasSelfDeterminationSkills ? 'Yes' : 'No'));
+  })));
+};
 
 module.exports = data => React.createElement(PreEtsReport, {
   data: data
--- reportTemplatesSrc/preEtsReport.js
+++ reportTemplatesSrc/preEtsReport.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const { Component } = React;
+const chunk = require('lodash/chunk');
 const SingleTermReportTitle = require('./components/SingleTermReportTitle');
 
 class PreEtsReport extends Component {
@@ -12,6 +13,8 @@ class PreEtsReport extends Component {
       appliedFilters
     } = this.props.data;
 
+    const studentPages = chunk(inSchoolStudents, 6);
+
     return (
       <React.Fragment>
         <SingleTermReportTitle
@@ -21,103 +24,7 @@ class PreEtsReport extends Component {
           term={term}
           appliedFilters={appliedFilters}
         />
-        <table
-          style={{
-            width: '100%',
-            textAlign: 'center',
-            borderCollapse: 'collapse'
-          }}
-        >
-          <thead>
-            <tr
-              style={{
-                backgroundColor: '#D43425',
-                color: 'white',
-                width: '100%',
-                fontSize: '50%',
-                padding: '20px 10px 20px 10px'
-              }}
-            >
-              <th>Name</th>
-              <th>Student ID</th>
-              <th>
-                Race/
-                <br />
-                Ethnicity
-              </th>
-              <th>Engaged with VR</th>
-              <th>On-track for graduation</th>
-              <th>Job Exploration Counseling</th>
-              <th>Work Based Learning</th>
-              <th>
-                Counseling on Transition
-                <br />
-                Programs or PSE at IHEs
-              </th>
-              <th>
-                Training in Workplace Readiness,
-                <br />
-                Social Skills,
-                <br />
-                Independent Living
-              </th>
-              <th>Instruction in Self-Advocacy</th>
-            </tr>
-          </thead>
-          <tbody>
-            {inSchoolStudents.map((student, index) => {
-              const engagedWithVr = student.activities.some(
-                activity =>
-                  activity.activityType.name ===
-                  'Referral Complete to VR (PW, IAC)'
-              );
-              const hasJobExplorationCounseling = student.activities.some(
-                activity =>
-                  activity.activityType.name ===
-                  'Job Exploration Counseling (CW, Pre-ETS)'
-              );
-              const hasWorkBasedLearning = student.activities.some(
-                activity =>
-                  activity.activityType.activityTypeGroup.name ===
-                  'Work Experience'
-              );
-
-              const hasCounselingOnTransitionPrograms = student.activities.some(
-                activity => {
-                  const activityTypeName = activity.activityType.name;
-                  return (
-                    activityTypeName === 'Career Mentor (CW, SS)' ||
-                    activityTypeName === 'Graduation Coach/ Mentor (SS)' ||
-                    activityTypeName ===
-                      'School Counselor for Post-School Planning (SS)'
-                  );
-                }
-              );
-
-              return (
-                <TableRow key={student.id} index={index}>
-                  <td>
-                    {student.firstName} {student.lastName}
-                  </td>
-                  <td>{student.studentId}</td>
-                  <td>{student.race}</td>
-                  <td>{engagedWithVr ? 'Yes' : 'No'}</td>
-                  <td>{student.onTrack ? 'Yes' : 'No'}</td>
-                  <td>{hasJobExplorationCounseling ? 'Yes' : 'No'}</td>
-                  <td>{hasWorkBasedLearning ? 'Yes' : 'No'}</td>
-                  <td>{hasCounselingOnTransitionPrograms ? 'Yes' : 'No'}</td>
-                  <td>
-                    {student.hasIndependentLivingSkills ||
-                    student.hasSocialSkills
-                      ? 'Yes'
-                      : 'No'}
-                  </td>
-                  <td>{student.hasSelfDeterminationSkills ? 'Yes' : 'No'}</td>
-                </TableRow>
-              );
-            })}
-          </tbody>
-        </table>
+        {studentPages.map(students => <PreETSTable inSchoolStudents={students} />)}
       </React.Fragment>
     );
   }
@@ -136,5 +43,106 @@ function TableRow(props) {
     />
   );
 }
+
+const PreETSTable = ({ inSchoolStudents, ...rest }) => (
+  <table
+    style={{
+      width: '100%',
+      textAlign: 'center',
+      borderCollapse: 'collapse'
+    }}
+    {...rest}
+  >
+    <thead>
+      <tr
+        style={{
+          backgroundColor: '#D43425',
+          color: 'white',
+          width: '100%',
+          fontSize: '50%',
+          padding: '20px 10px 20px 10px'
+        }}
+      >
+        <th>Name</th>
+        <th>Student ID</th>
+        <th>
+          Race/
+        <br />
+          Ethnicity
+      </th>
+        <th>Engaged with VR</th>
+        <th>On-track for graduation</th>
+        <th>Job Exploration Counseling</th>
+        <th>Work Based Learning</th>
+        <th>
+          Counseling on Transition
+        <br />
+          Programs or PSE at IHEs
+      </th>
+        <th>
+          Training in Workplace Readiness,
+        <br />
+          Social Skills,
+        <br />
+          Independent Living
+      </th>
+        <th>Instruction in Self-Advocacy</th>
+      </tr>
+    </thead>
+    <tbody>
+      {inSchoolStudents.map((student, index) => {
+        const engagedWithVr = student.activities.some(
+          activity =>
+            activity.activityType.name ===
+            'Referral Complete to VR (PW, IAC)'
+        );
+        const hasJobExplorationCounseling = student.activities.some(
+          activity =>
+            activity.activityType.name ===
+            'Job Exploration Counseling (CW, Pre-ETS)'
+        );
+        const hasWorkBasedLearning = student.activities.some(
+          activity =>
+            activity.activityType.activityTypeGroup.name ===
+            'Work Experience'
+        );
+
+        const hasCounselingOnTransitionPrograms = student.activities.some(
+          activity => {
+            const activityTypeName = activity.activityType.name;
+            return (
+              activityTypeName === 'Career Mentor (CW, SS)' ||
+              activityTypeName === 'Graduation Coach/ Mentor (SS)' ||
+              activityTypeName ===
+              'School Counselor for Post-School Planning (SS)'
+            );
+          }
+        );
+
+        return (
+          <TableRow key={student.id} index={index}>
+            <td>
+              {student.firstName} {student.lastName}
+            </td>
+            <td>{student.studentId}</td>
+            <td>{student.race}</td>
+            <td>{engagedWithVr ? 'Yes' : 'No'}</td>
+            <td>{student.onTrack ? 'Yes' : 'No'}</td>
+            <td>{hasJobExplorationCounseling ? 'Yes' : 'No'}</td>
+            <td>{hasWorkBasedLearning ? 'Yes' : 'No'}</td>
+            <td>{hasCounselingOnTransitionPrograms ? 'Yes' : 'No'}</td>
+            <td>
+              {student.hasIndependentLivingSkills ||
+                student.hasSocialSkills
+                ? 'Yes'
+                : 'No'}
+            </td>
+            <td>{student.hasSelfDeterminationSkills ? 'Yes' : 'No'}</td>
+          </TableRow>
+        );
+      })}
+    </tbody>
+  </table>
+)
 
 module.exports = data => <PreEtsReport data={data} />;
