730ca48caa07854e6f02b184a6fc041a1a2af70a
Justin Felt <justin.felt@emberex.com>
2020-01-24 09:52:20 -0800

    Client build

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a991cf994ce4d5ce1b31a52f32fed37.js"
+  "/precache-manifest.4b8ede64c2d9f736feb532c29d9fbe62.js"
 );
 
 workbox.clientsClaim();
